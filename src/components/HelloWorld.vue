19ef59c5da6205cb8ebf5c314b9370123c214d9e
Alejandro Gonzalez Jimenez <alexgj@um.es>
2018-09-26 11:59:33 +0200

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Welcome</h1>
+    <h2>What's up</h2>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+console.log(sourceData)
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
+}
+</script>
