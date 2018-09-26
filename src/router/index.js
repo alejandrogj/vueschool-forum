a8aa656861b460e0b10784ddbe8bd2e289c48bd2
Alejandro Gonzalez Jimenez <alexgj@um.es>
2018-09-26 14:15:17 +0200

    Handling not found pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/PageHome'
 import ThreadShow from '@/pages/PageThreadShow'
+import PageNotFound from '@/pages/PageNotFound'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       name: 'ThreadShow',
       component: ThreadShow,
       props: true
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: PageNotFound
     }
   ],
   mode: 'history'
