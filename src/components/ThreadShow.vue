ed455873174d66354f574d15d1c3b179a0046e37
Alejandro Gonzalez Jimenez <alexgj@um.es>
2018-09-26 13:01:11 +0200

    Introducing the vue-router

---
+++ src/components/ThreadShow.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="col-large push-top">
+      <h1>{{thread.title}}</h1>
+      <div class="post-list">
+        <div v-for="postId in thread.posts" class="post">
+          <div class="user-info">
+            <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+            <a href="#" class="avatar-large">
+              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
+            </a>
+          </div>
+          <div class="post-content">
+            <div>
+              {{posts[postId].text}}
+            </div>
+          </div>
+          <div class="post-date text-faded">
+              {{posts[postId].publishedAt}}
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+export default {
+  props: {
+    id: {
+      required: true,
+      type: String
+    }
+  },
+  data () {
+    return {
+      thread: sourceData.threads[this.id],
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
+}
+</script>
+
