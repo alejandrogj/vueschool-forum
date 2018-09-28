94f14f812122f8bef2c0d1ff5e9a437376a68182
Alejandro Gonzalez Jimenez <alexgj@um.es>
2018-09-28 10:16:55 +0200

    Global Base Components

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -21,12 +21,9 @@
 <script>
 
 import sourceData from '@/data'
-import AppDate from './AppDate'
 
 export default {
-  components: {
-    AppDate
-  },
+
   props: {
     post: {
       required: true,
