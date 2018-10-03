729029a7bde9499f0868053e7b1347c9ac76ee79
Alejandro Gonzalez Jimenez <alexgj@um.es>
2018-10-03 13:47:26 +0200

    Categories, Collections of Forums

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/pages/PageHome'
 import ThreadShow from '@/pages/PageThreadShow'
 import Forum from '@/pages/PageForum'
 import PageNotFound from '@/pages/PageNotFound'
+import Category from '@/pages/PageCategory'
 
 Vue.use(Router)
 
@@ -18,6 +19,12 @@ export default new Router({
       path: '/thread/:id',
       name: 'ThreadShow',
       component: ThreadShow,
+      props: true
+    },
+    {
+      path: '/category/:id',
+      name: 'Category',
+      component: Category,
       props: true
     },
     {
